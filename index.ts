85bac2d491f384721985fb4b6e9edcf67bd08472
Robert-Martins <robertcantares@gmail.com>
2024-04-12 19:09:52 -0300

    feat/include-project-entry-point

---
+++ index.ts
@@ -0,0 +1,13 @@
+(() => 
+    console.log(
+        "                                Welcome                                  ",
+        "\n",
+        "=========================================================================\n",
+        "\n",
+        "In this app directory a TS implementation of Java's Optional can be found.\n",
+        "Tests with use cases can be found in the test folder.\n",
+        "To execute the testing, run the test command.\n",
+        "\n",
+        "=========================================================================\n",
+    ) 
+)();
